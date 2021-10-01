8332d058b1ef93c362e20c52a4bbfe55c20dd41d
Pooja Paliwal <60742005+poojapaliwal@users.noreply.github.com>
2021-10-01 22:59:25 +0530

    added code for counting_palindrome_substrings

---
+++ count_palindrome_substr.cpp
@@ -0,0 +1,41 @@
+//Problem statement : string s is given. find out the number of palindromic substrings formed from the given string.
+//   -> let s : "aba"   
+//   -> possible substrings : "a","b,","a","ab","ba","aba"
+//   -> total palindromic substrings :  "a","b","a","aba" : 4     ---> output
+
+#include<bits/stdc++.h>
+using namespace std;
+int countPalisubstr(string s){
+
+    int count=0;
+    int dp[s.size()][s.size()];
+    memset(dp,-1,sizeof(dp));
+    for(int i=0;i<s.size();i++){
+        int j=i;
+        dp[i][j] = 1;
+        count++;
+    }
+    for(int i=0;i<s.size()-1;i++){
+        int j=i+1;
+        if(s[i]==s[j]){
+            dp[i][j] = 1;
+            count++;
+        }
+    }
+    for(int k=2;k<s.size();k++){
+        for(int i=0;i<s.size()-2;i++){
+            if(dp[i+1][i+k-1]==1 && s[i]==s[i+k]){
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    int n = countPalisubstr(s);
+    cout<<n;
+}
